8f63f2728ddeb279b12531b6f3ab1b92519ded05
AhmedMoneimm <ahmedmoneimaly@gmail.com>
2023-05-21 13:34:25 +0300

    [Fix] in main, filter was always FIR

--- FIR_wave_filter.m
+++
@@ -1,7 +0,0 @@
-function filtered_wave = FIR_wave_filter(fs, f_low, f_high, input_wave, gain)
-    filter_order = 100;
-    a = 1;
-    filter_cutoff = [f_low, f_high]/(fs/2);
-    b = fir1(filter_order, filter_cutoff, 'bandpass');
-    filtered_wave = applyBandPassFilter(fs, input_wave, b, a, gain, f_low, f_high);
-end
